d49d37b1651913cde77b5078870d3835d7727d1d
ELMER015A <56976465+elmer015a@users.noreply.github.com>
2019-11-03 18:52:42 -0500

    SE AGREGO EL TRABAJO 04

---
+++ TRABAJO04.py
@@ -0,0 +1,329 @@
+# Ejercicio 01
+# calcular la velocidad
+distancia,velocidad,tiempo=0.0,0.0,0.0
+
+# asignacion de valores
+distancia=400
+tiempo=20
+
+# calculo
+velocidad=distancia/tiempo
+
+# mostrar valores
+print(" distancia es", distancia)
+print(" tiempo es", tiempo)
+print("velocidad es", velocidad)
+
+# Ejercicio 02
+# calcular el area del circulo
+radio,pi,area=0.0,3.14,0.0
+
+# asignacion de valores
+radio=8
+pi=3.14
+
+# calculo
+area=(radio*radio)*pi
+
+# mostrar valores
+print(" radio es",radio)
+print(" pi es",pi)
+print("area es", area)
+
+# Ejercicio 03
+# calcular volumen de un cubo
+lado1,lado2,lado3,volumen=0.0,0.0,0.0,0.0
+
+# asignacion de valores
+lado1=10
+lado2=7
+lado3=15
+
+# calculo
+volumen=lado1*lado2*lado3
+
+# mostrar valores
+print(" lado1 es",lado1)
+print(" lado2 es", lado2)
+print(" lado3 es",lado3)
+print("volumen es", volumen)
+
+# Ejercicio 04
+# calcular el area del cuadrado
+lado,Area=0.0,0.0
+
+# asignacion de valores
+lado=8
+
+# calculo
+Area=lado*lado
+
+# mostrar valores
+print(" lado es", lado)
+print("Area es", Area)
+
+# Ejercicio 05
+# calcular el valor de E
+a,b,c,E=0.0,0.0,0.0,0.0
+
+# asignacion de valores
+a,b,c=30,15,12.5
+
+#calculo
+E=a+b-c
+
+# mostrar valores
+print(" a es", a)
+print(" b es", b)
+print(" c es", c)
+print("E es",E)
+
+# Ejercicio 06
+# calcular el valor M
+M,x,y,z,p=0.0,0.0,0.0,0.0,0.0
+
+# asignacion de valores
+x,y,z,p=40,8,48,16
+
+#calculo
+M=(x/y)*(z/p)
+
+# mostrar valores
+print(" x es",x)
+print(" y es", y)
+print(" z es ", z)
+print(" p es", p)
+print("M es", M)
+
+# Ejercicio 07
+# calcular el pago de de un campesino a la semanana
+dias,cantidad,pago=0.0,0.0,0.0
+
+# asigbacion de valores
+dias=6
+cantidad=35
+
+#calculo
+pago=dias*cantidad
+
+# motrar valores
+print(" dias es", dias)
+print(" cantidad es", cantidad)
+print("pago es", pago)
+
+# Ejercicio 08
+# cacular la fuerza
+fuerza,masa,aceleracion=0.0,0.0,0.0
+
+# asignacion valores
+masa=15
+aceleracion=0.5
+
+#calculo
+fuerza=masa*aceleracion
+
+# mostrar valores
+print(" masa es",masa)
+print(" aceleracion es", aceleracion)
+print("fuerza es",fuerza)
+
+# Ejercicio 09
+# calcular el IGV  de una compra
+igv,precio,porcentaje=0.0,0.0,0.0
+
+# asignacion de valores
+precio=335
+porcentaje_de_igv=0.18
+
+# calculo
+igv=precio*porcentaje_de_igv
+
+# mostra valores
+print(" precio es",precio)
+print(" porcentaje de igv es", porcentaje_de_igv)
+print("igv es ",igv)
+
+# Ejercicio 10
+# calcular el area de un trapecio
+base_mayor,base_menor,atura,area=0.0,0.0,0.0,0.0
+
+# asignacion de valores
+base_mayor=20
+base_menor=10
+altura=8
+
+#calculo
+area=((base_menor + base_mayor)/2)*altura
+
+# mostrar valores
+print(" base menor es", base_menor)
+print(" base mayor es", base_mayor)
+print(" altura es",altura)
+print("area es",area)
+
+# Ejercicio 11
+# calcular la energia
+energia,velocidad_de_luz,masa=0.0,0.0,0.0
+
+# asignacion de valores
+velocidad_de_luz=300000000
+masa=12
+
+# calculo
+energia=masa*velocidad_de_luz
+
+# mostrar valores
+print(" masa es",masa)
+print(" velocidad de luz es",velocidad_de_luz)
+print("energia es", energia)
+
+# Ejercicio 12
+# calcular el valor de x
+x,p,r,s,q=0.0,0.0,0.0,0.0,0.0
+
+# asignacion de valores
+p,q,r,s=18,19.5,25,20
+
+# calculo
+x=(p*s)/r+q
+
+# mostrar valores
+print(" p es",p)
+print(" q es",q)
+print(" r es",r)
+print(" s es",s)
+print("x es ",x)
+
+# Ejercicio 13
+# calcular el area de un rectangulo
+area_rectangulo,base,altura=0.0,0.0,0.0
+
+# asignacion de valores
+base=14
+altura=8
+
+# calculo
+area_rectangulo=base*altura
+
+# mostrar valores
+print(" base es", base)
+print(" altura es", altura)
+print("area rectangulo es",area_rectangulo)
+
+# Ejercicio 14
+# calcular el pago de un empleado con su descuento de seguro
+pago,descuento,recibo=0.0,0.0,0.0
+
+# asignacion de valores
+pago=1200
+descuento=0.8
+
+# calculo
+recibo=pago*descuento
+
+# mostrar valores
+print(" pago es",pago)
+print(" descuento es",descuento)
+print("recibo es",recibo)
+
+# Ejercicio 15
+# calcular la suma de cuadrados
+s,a,b=0.0,0.0,0.0
+
+# asignacion de valores
+a=8
+b=2
+
+# calculo
+s=((a*a)+(b*b)+(a*b))
+
+#mostrar valores
+print(" a es", a)
+print(" b es",b)
+print("s es",s)
+
+# Ejercicio16
+# calcular la cantidad recaudada de una panaderia al dia
+total_pan,precio,recaudado=0.0,0.0,0.0
+
+# asignacion de valores
+total_pan=300
+precio=0.2
+
+# calculo
+recaudado=total_pan*precio
+
+# mostrar valores
+print(" total de pan es", total_pan)
+print(" precio de cada pan es",precio)
+print("recaudado del dia es",recaudado)
+
+# Ejercicio 17
+# calcular el siguiente valor
+R,a,b=0.0,0.0,0.0
+
+# asigancion de valores
+a=2
+b=5
+
+# calculo
+R=((a**b)+(b**a)+(a*b))/2
+
+# mostrar valores
+print(" a es",a)
+print(" b es",b)
+print(" R es",R)
+
+# Ejercicio 18
+# calcular lo siguiente
+porcentaje,ingreso,total,recibo=0.0,0.0,0.0,0.0
+
+# asigancion de valores
+ingreso=3000
+porcentaje_descuento=0.15
+pago_tarjeta=30
+
+#calculo
+recibo=(ingreso*porcentaje_descuento)-(pago_tarjeta)
+
+# mostrar valores
+print(" pago de tarjeta es",pago_tarjeta)
+print(" porcentaje de descuento es",porcentaje_descuento)
+print(" ingreso es",ingreso)
+print("recibo es", recibo)
+
+# EJERCICIO 19
+# calcular el valor P
+p,M,N,Q,R,S=0.0,0.0,0.0,0.0,0.0,0.0
+
+# asignacion de valores
+M,N,Q,R,S=36,3,8,2,5
+
+# calculo
+P=((M/N)*R)/S
+
+# mostra valores
+print(" M es", M)
+print(" N es", N)
+print(" Q es", Q)
+print(" R es", R)
+print(" S es", S)
+print("P es", P)
+
+# Ejercicio 20
+# calcular la velocidad final de un automovil
+velocidad_final,velocidad_inicial,aceleracion,tiempo=0.0,0.0,0.0,0.0,
+
+#asiganacion de valores
+velocidad_inicial=15
+aceleracion=8
+tiempo=2
+
+# calculo
+velocidad_final=velocidad_inicial+aceleracion*tiempo
+
+# mostra valores
+print(" velocidad inicial es", velocidad_inicial)
+print(" aceleracion es", aceleracion)
+print(" tiempo es",tiempo)
+print("velocidad final es",velocidad_final)
